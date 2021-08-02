72f0fcdf7fac5a6beb7d6ad0c4248d58669d3432
DuckyHacker <yuhi.baske0222@i.softbank.jp>
2021-08-02 19:19:48 +0900

    workflow_EC2へデプロイするように修正

--- src/main/java/com/example/smabro_app/presentation/controller/api/MatchingController.java
+++ src/main/java/com/example/smabro_app/presentation/controller/api/MatchingController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/matches")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 public class MatchingController {
 
     private final MatchingFacade matchingFacade;
